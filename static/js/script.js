ae2d0e51181466c5e242f31dae0704178668805f
Steve Hanlon <rshanlon@gmail.com>
2017-06-23 19:42:17 -0700

    Collection's collectible  modal window working with each picture showing it's correct details.  Still needs CSS for layout.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,20 @@
 /*
- Created on.
+ Created on June 16th by Steve Hanlon.  JS file for collection app.
 */
 
 
+
+/*---------------------------
+ MODAL WINDOW - DETAIL VIEW
+-----------------------------*/
+
+// Show the Modal window
+$('.myBtn').on('click', function() {
+    let somevar = $(this).attr("data-collectible");
+    $('#galleryModal_' + somevar).css('display', 'block');
+});
+
+// Hide the modal window
+$('.close').on('click', function() {
+    $('.modal').css('display','none');
+});
